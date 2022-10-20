9684d3fcf77cff1b18d5a5a7ba3d31c0b0ea0120
Juanmaponce <65909673+Juanmaponce@users.noreply.github.com>
2022-10-20 12:18:43 -0300

    Add project files.

---
+++ Juan_Manuel_Ponce_Casting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juan_Manuel_Ponce_Casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
